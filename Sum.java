ba5d0fcb78d6e1d1560d3e4f8d87efea2402ccf5
Anurag Gaiwal <anuraggaiwal@gamil.com>
2023-01-25 15:36:59 +0530

    Commited

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 15;
+        System.out.println("Sum of a and b is: " + (a + b));
+
+    }
+
+}
